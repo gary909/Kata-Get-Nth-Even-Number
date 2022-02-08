1b4beefe674401308e219783188c5d359f2fac59
gary <gary.white@lodgit.com>
2022-02-08 13:11:57 +0100

    code

---
+++ index.js
@@ -0,0 +1,9 @@
+function nthEven(n){
+    // your code here
+}
+
+console.log(nthEven(1)); // 0
+console.log(nthEven(2)); // 2
+console.log(nthEven(3)); // 4
+console.log(nthEven(100)); // 198
+console.log(nthEven(1298734)); // 2597466
